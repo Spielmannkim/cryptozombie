437b985ac79ba0f20ca34f000da48a9cd02ea0ec
spielmannkim <shinjongk@gmail.com>
2023-03-17 17:47:33 +0900

    private, view, retruns

--- lesson_01/zombieFactory.sol
+++ lesson_01/zombieFactory.sol
@@ -17,6 +17,13 @@ contract ZombieFactory {
     function _createZombie(string _name, uint _dna) private { //private 함수는 관례상 함수명앞에_를 붙여준다. // 좀비생성(고유이름,dna를 갖음)
     // 이 함수가 실행되면 Zombie가 생성되어 zombies배열에 차례대로 추가된다.
     zombies.push(Zombie(_name, _dna));
+    // public은 누구나 함수를 호출할 수 있음
+    // private은 컨트랙트 내의 다른 함수들만 이 함수를 호출 할 수 있음.
+    // view는 함수가 데이터를 보기만 하고 변경하지 않는다는 뜻.
+    // pure는 함수가 앱에서 어떤 데이터도 접근하지 않는다는것을 의미.
+    function _generateRandomDna(string _str) private view returns (uint){
+
+    }
 
     } 
 
