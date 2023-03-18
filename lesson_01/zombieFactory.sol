83d0d1d9f1bb638e64b7eb061a00cf2de9cce30c
spielmannkim <shinjongk@gmail.com>
2023-03-18 22:19:30 +0900

    이벤트

--- lesson_01/zombieFactory.sol
+++ lesson_01/zombieFactory.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.19;
 
 contract ZombieFactory {
+    // 좀비가 생성될때마다 사용자단에서 이에대해 알고 표시하도록 하는 이벤트
+    event NewZombie(uint zombieId, string name, uint dna);
     // uint는 음수가 아닌 정수
     // 6번째줄 코드는 상태 변수로 컨트랙트에 영구적으로 저장됨.
     uint dnaDigits = 16;
@@ -15,8 +17,13 @@ contract ZombieFactory {
     Zombie[] public zombies;
     // 함수 선언
     function _createZombie(string _name, uint _dna) private { //private 함수는 관례상 함수명앞에_를 붙여준다. // 좀비생성(고유이름,dna를 갖음)
-    // 이 함수가 실행되면 Zombie가 생성되어 zombies배열에 차례대로 추가된다.
-    zombies.push(Zombie(_name, _dna));
+    
+    // 배열의 마지막(최신좀비)를 uint id변수에 저장
+    uint id = zombies.push(Zombie(_name, _dna)) - 1;
+    // 위에서 선언한 이벤트를 여기서 실행한다
+    NewZombie(id, _name, _dna);
+
+
     // public은 누구나 함수를 호출할 수 있음
     // private은 컨트랙트 내의 다른 함수들만 이 함수를 호출 할 수 있음.
     // view는 함수가 데이터를 보기만 하고 변경하지 않는다는 뜻.
